5f911e8eb0b360d81357b7028ae552d4b4ceb926
Denis <92470685+denis02107@users.noreply.github.com>
2023-09-05 22:59:39 +0300

    Create ct1.cs
    1 and 2

---
+++ ct1.cs
@@ -0,0 +1,56 @@
+public class MathOperations
+{
+    public int SUM(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int SUM(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    public int SUM(int a, int b, int c, int d)
+    {
+        return a + b + c + d;
+    }
+}
+public class Plant
+{
+    public int Height { get; set; }
+    public int Weight { get; set; }
+    public int Width { get; set; }
+
+    public Plant(int height, int weight) : this(height, weight, 0)
+    {
+
+    }
+
+    public Plant(int height, int weight, int width)
+    {
+        this.Height = height;
+        this.Weight = weight;
+        this.Width = width;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Height: {Height}, Weight: {Weight}, Width: {Width}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        MathOperations operations = new MathOperations();
+        Console.WriteLine(operations.SUM(2, 3));
+        Console.WriteLine(operations.SUM(2, 3, 4));
+        Console.WriteLine(operations.SUM(2, 3, 4, 5));
+
+        Plant plant1 = new Plant(150, 2);
+        Plant plant2 = new Plant(180, 3, 50);
+
+        plant1.DisplayInfo();
+        plant2.DisplayInfo();
+    }
+}
